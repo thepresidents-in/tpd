56e267ea648d296ea329faf0c1dfed4e893367bf
Khushboo Vijayvargiya <Khushboo.Vijayvargiya@PTPL.Local>
2019-07-05 15:57:04 +0530

    Receipt print

--- src/app/receipt/receipt-detail.component.ts
+++ src/app/receipt/receipt-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router,ActivatedRoute } from '@angular/router';
+import { RestService } from '../rest.service';
 
 @Component({
   selector: 'app-receipt-detail',
@@ -7,8 +9,25 @@ import { Component } from '@angular/core';
 })
 
 export class ReceiptDetailComponent {
-	constructor() {
-		console.log("ReceiptDetailComponent");
+	receiptId: string;
+	receiptDetail:any;
+	studentDetail:any;
+	constructor(private route: ActivatedRoute,private rest: RestService) {
 	}
   
+  ngOnInit() {
+		this.route.params.subscribe(params => {
+		this.receiptId =  params['id'];
+		});
+
+		this.rest.getReceiptById(this.receiptId).then((receiptDetails) => {
+		this.receiptDetail = receiptDetails;
+		console.log("res details class: "+receiptDetails[0]['class']);
+		console.log("res details roll_no: "+receiptDetails[0]['roll_no']);
+		this.rest.selectAllByClassAndRollNum(receiptDetails[0]['class'],parseInt(receiptDetails[0]['roll_no'])).then((studentDetails) =>{
+			this.studentDetail = studentDetails;
+			  console.log("studentDetails: ",studentDetails);
+		  })
+		});
+    }
 }
