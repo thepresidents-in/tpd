173b5f758ba1643063009d5d05dfcfc4f2d71ce8
Khushboo Vijayvargiya <Khushboo.Vijayvargiya@PTPL.Local>
2019-10-17 14:21:31 +0530

    student detail added

--- src/app/student/studentDetail.component.ts
+++ src/app/student/studentDetail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestService } from '../rest.service';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-student-detail',
@@ -6,8 +9,22 @@ import { Component, OnInit, Inject } from '@angular/core';
   styleUrls: ['./studentDetail.component.css']
 })
 
-export class StudentDetailComponent  {
-	constructor(){
+export class StudentDetailComponent implements OnInit {
+	studentData: any[];
+	constructor(private route: ActivatedRoute,private rest: RestService,private spinnerService: Ng4LoadingSpinnerService){
 	console.log("StudentDetailComponent");
+}
+	ngOnInit() {
+		this.spinnerService.show();
+		this.route.params.subscribe(params => {
+        console.log("std id: "+[params['id']]);
+        this.rest.getStudentsById(params['id']).then((response) => {
+          console.log("res KV: ",response);
+          this.studentData = response ;
+          console.log("studentData detail :",this.studentData);
+          this.spinnerService.hide();
+        });
+    });
 	}
+	
 }
